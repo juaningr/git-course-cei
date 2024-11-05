246d22fc1a75a0f0b3918f4225792759fa21dd96
JuanGR <j.gallegor@upm.es>
2024-11-05 13:01:13 +0100

    Updated python program to use submodules

--- program.py
+++ program.py
@@ -1,4 +1,10 @@
+import sys
+
+sys.path.append("./git-course-submodule/")
+
+from program_s import call_this
+
 my_list = [21, 45, 39, 11]
 
 for index, val in enumerate(my_list):
-    print(index, val)
+    print(index, call_this(val))
